0df1ad29c3074e08a778ff32c879bbd23a45b7ab
Your Name <michaeladeleye365@gmail.com>
2025-12-04 21:53:28 +0000

    Adjusting things

--- Landing-Page/src/components/NotFound.jsx
+++ Landing-Page/src/components/NotFound.jsx
@@ -11,6 +11,9 @@ const NotFound = () => {
         <span className="digit">4</span>
         <span className="zero-circle">0</span>
         <span className="digit">4</span>
+        {/* <span class="digit">4</span>
+        <span class="digit">0</span>
+        <span class="digit">4</span>*/}
       </h1>
       <h2 className="path">
         404 - <span>{location.pathname}</span>
